3e8af7748d9b98b8cec7f2ee0fff6b0cde334056
Farhan Wundari <squidwardsus@Squidwardsus.local>
2025-04-29 22:18:28 +0700

    third commint

--- src/route/public-route.js
+++ src/route/public-route.js
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { forgotPassword, googleAuth, login, register, resetPassword } from '../controller/user-controller.js';
+import { getAllProducts } from '../controller/product-controller.js';
 
 
 
@@ -10,5 +11,7 @@ publicRouter.post('/api/v1/auth/login', login);
 publicRouter.post('/api/v1/auth/forgot-password', forgotPassword);
 publicRouter.post('/api/v1/auth/reset-password', resetPassword);
 publicRouter.post('/api/v1/auth/google', googleAuth);
+
+publicRouter.get('/api/v1/products', getAllProducts);
 
 export default publicRouter
